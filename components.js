11d592c9a8df6e8679fd3f6ae2747efd2c91e25d
renatobarramansa <rbvbarra@gmail.com>
2022-09-13 13:35:17 -0300

    [feat:] Inserção do topo com o components
    Mudamos o import do script comum, para o módulo components.js

---
+++ components.js
@@ -0,0 +1,5 @@
+//Importando o js do submodule
+import * as module from "./components/src/main.js"
+const dirComponent = './components'
+module.elementosSide(dirComponent,['add.png'],['./'])
+module.topo(dirComponent,"Captação")

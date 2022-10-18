56d371e1752ac221be9b8eadce835d90dc73181e
Gabriel.Alves <gabrieljorge343@gmail.com>
2022-10-18 17:27:01 -0300

    [ref]: modificando main para frontEnd.....    Amém

---
+++ header.php
@@ -0,0 +1,12 @@
+<header class="topo">
+        
+  <div class="faixaVerm">
+      <span id="departamento"></span>
+  </div>
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/escudo.png" alt="">         
+  <div class="faixaBranca">
+      <h1>FLUMINENSEFC</h1>    
+  </div>
+  
+  <div class="faixaVerde"></div>
+</header>

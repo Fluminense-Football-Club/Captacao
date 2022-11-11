1e802b7bfdbbf2a843f28ea3e83cb23ae7d5db81
renatobarramansa <rbvbarra@gmail.com>
2022-11-11 17:19:37 -0300

    [feat]:Inserção linha vertical nos cards dos aplicativos

--- home-captacao.php
+++ home-captacao.php
@@ -27,25 +27,34 @@
       </div>
       <!-- <div class="apps"> -->
         <ul class="apps">
-          <li>
+          <li>   
+          <div class="linhaVertical-green"></div>  
+          <div>
             <a href="/captacao/cadastro">
               <img src="<?php echo get_stylesheet_directory_uri(); ?>/Captacao/assets/add.svg" alt="">
               <span>Cadastrar atleta</span>
             </a>
+            </div>         
           </li>
 
-          <li>
-            <a href="/captacao/fichas">
+          <li>   
+          <div class="linhaVertical-yellow"></div>  
+          <div>
+            <a href="/captacao/cadastro">
               <img src="<?php echo get_stylesheet_directory_uri(); ?>/Captacao/assets/settings.svg" alt="">
-              <span>Editar atleta</span>
+              <span>Visualizar fichas</span>
             </a>
+            </div>         
           </li>
 
-          <li>
+          <li>   
+          <div class="linhaVertical-red"></div>  
+          <div>
             <a href="/captacao/graficos">
               <img src="<?php echo get_stylesheet_directory_uri(); ?>/Captacao/assets/grafico.svg" alt="">
-              <span>Graficos</span>
+              <span>Visualizar Gráficos</span>
             </a>
+            </div>         
           </li>
         </ul>
       <!-- </div> -->
@@ -60,5 +69,8 @@
      </div>
     </section>
    <?php endif; ?>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>

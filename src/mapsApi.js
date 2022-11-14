0acdc2e79a30e780e12b5d76ca996e0151216593
Gabriel.Alves <gabrieljorge343@gmail.com>
2022-11-14 12:44:58 -0300

    Feat: Criar mapa com nome de captadores nos estados.

--- src/mapsApi.js
+++ src/mapsApi.js
@@ -1,16 +1,43 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
+  const Brasil = { lat: -14.235004, lng: -51.92528 };
+
+  const Estados = [
+    ['captador1',{ lat: -22.9068467, lng: -43.1728965 }],
+    ['captador2',{ lat: 1.5957682, lng: -60.58206759999999 }],
+    ['captador3',{ lat: -5.402580299999999, lng: -36.954107 }],
+  ];
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: Brasil,
   });
   // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+  Estados.forEach((item) => {
+    let nome
+    let local
+    [nome, local] = item
+    let caixa = document.createElement('div')
+    caixa.innerText = nome
+
+    let marker = new google.maps.Marker({
+      position: local,
+      map: map,
+    });
+
+    let infowindow = new google.maps.InfoWindow({
+      content: caixa,
+      ariaLabel: "teste",
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+
   });
 }
 window.initMap = initMap;

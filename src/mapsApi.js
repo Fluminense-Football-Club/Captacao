5ad710bb0b79450627149e46573489fb32ca90e4
Gabriel.Alves <gabrieljorge343@gmail.com>
2022-11-18 10:45:20 -0300

    [fix]: Link do css comentado

--- src/mapsApi.js
+++ src/mapsApi.js
@@ -17,6 +17,7 @@ function initMap_Captadores() {
     ["captador3", { lat: -5.402580299999999, lng: -36.954107 }],
   ];
   // Create markers of Capitadores
+  let dados_captadores = [['Rio de Janeiro',15,5],['Roraima',20,7],['Rio Grande do Norte',12,9]]
   relacao_captadores.forEach((item, index) => {
     let nome;
     let local;
@@ -33,9 +34,9 @@ function initMap_Captadores() {
     let totalAprovados = document.createElement("p");
 
     nomeCaptador.innerText = nome;
-    endCaptador.innerText = "Endereço:";
-    totalIndicados.innerText = "Total indicados";
-    totalAprovados.innerText = "Total aprovados";
+    endCaptador.innerText = `Endereço: ${dados_captadores[index][0]}`;
+    totalIndicados.innerText = `Total indicados: ${dados_captadores[index][1]}`;
+    totalAprovados.innerText = `Total aprovados: ${dados_captadores[index][2]}`;
 
     caixa.appendChild(img);
     caixa.appendChild(caixa2);
@@ -220,11 +221,12 @@ function initMap_Atletas() {
   });
 
   const relacao_captadores = [
-    ["Atleta1", { lat: -22.9068467, lng: -43.1728965 }],
-    ["Atleta2", { lat: 1.5957682, lng: -60.58206759999999 }],
-    ["Atelta3", { lat: -5.402580299999999, lng: -36.954107 }],
+    ["Fred", { lat: -22.9068467, lng: -43.1728965 }],
+    ["Marcelo", { lat: 1.5957682, lng: -60.58206759999999 }],
+    ["Tiago Silva", { lat: -5.402580299999999, lng: -36.954107 }],
   ];
   // Create markers of Capitadores
+  let fotos_atletas = ['https://images2.minutemediacdn.com/image/upload/c_fill,w_1080,ar_16:9,f_auto,q_auto,g_auto/shape%2Fcover%2Fsport%2FFBL-LIBERTADORES-RIVER-FLUMINENSE-8ceb19b695c247bc0bae83122a59d177.jpg','https://s2.glbimg.com/Endj-NmHQ6maYDXq0reNNh06p8M=/0x0:2047x1256/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/P/9/yrBroxQFytqMmPK9znzw/26974672723-8b43b25245-k.jpg',"https://i.pinimg.com/564x/f1/3b/9e/f13b9e47a6d45a42d3b80ff4fb5aadba.jpg"]
   relacao_captadores.forEach((item, index) => {
     let nome;
     let local;
@@ -235,7 +237,7 @@ function initMap_Atletas() {
     caixa.classList.add("modal-captador");
 
     let img = document.createElement("img");
-    img.src = directory_uri + "/Captacao/assets/" + (index + 1) + ".PNG";
+    img.src =fotos_atletas[index];
     let nomeCaptador = document.createElement("p");
 
     nomeCaptador.innerText = nome;
@@ -254,7 +256,7 @@ function initMap_Atletas() {
       
       let div_botoes = document.createElement("div");
       div_botoes.onclick = () => {
-        window.location.href = "http://teste";
+        window.location.href = "https://www.inf.pucrs.br/~cnunes/ferramentas/Aulas/tipos_de_graficos.pdf";
       };
       let endCaptador = document.createElement("p");
       let totalIndicados = document.createElement("p");
